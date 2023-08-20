dec231955159c8e4c352e19ff6af8497416070d2
mahdi hosseini <hosseinim018@gmail.com>
2023-08-20 18:11:35 +0330

    Refactor Range class for improved readability, documentation, and best practices
    - Improve variable names for better readability and clarity
    - Add JSDoc-style comments for better documentation of constructor and draw method
    - Use const instead of let for variables that don't need reassignment
    - Follow consistent coding style and indentation
    - Use strict equality (===) to compare values instead of loose equality (==)
    - Update import statement for the trendline module
    - Split drawing logic into smaller steps for better readability
    
    The updated Range class provides a clear and well-documented implementation for representing a range shape on a canvas. The code has been refactored to enhance readability, maintainability, and adherence to best practices.

--- range.js
+++ range.js
@@ -1,26 +1,85 @@
-import {trendline} from "@/assets/chart/shapes/lines";
+/**
+ * This module provides a Range class for drawing and manipulating range shapes on a canvas.
+ * It extends the trendline class and includes methods for handling mouse events, updating
+ * the range's position and dimensions, and rendering the range on the canvas.
+ *
+ * Usage:
+ *  1. Import the module into your JavaScript file:
+ *     import { Range } from 'path/to/range-module';
+ *
+ *  2. Create an instance of the Range class, providing the initial x and y coordinates of
+ *     the two points that define the range:
+ *     const range = new Range(x1, x2, y1, y2);
+ *
+ *  3. Optional: Enable dragging by setting the `dragEnabled` property to true:
+ *     range.dragEnabled = true;
+ *
+ *  4. Add event listeners to handle user interactions, such as mouse clicks and movement:
+ *     canvas.addEventListener('mousedown', range.mousedown.bind(range));
+ *     canvas.addEventListener('mousemove', range.mousemove.bind(range));
+ *     canvas.addEventListener('mouseup', range.mouseup.bind(range));
+ *
+ *  5. Call the `draw` method of the range instance to render the range on the canvas:
+ *     range.draw(canvas);
+ *
+ *  6. Optional: Customize the appearance of the range by modifying properties like `fillColor`.
+ *
+ * Note: The `canvas` parameter should be an HTMLCanvasElement object.
+ */
+import { trendline } from "./lines";
 
-export class Range extends trendline{
-    constructor(x1, x2, y1, y2) {
-        super(x1, x2, y1, y2);
+/**
+ * Represents a Range shape on a canvas, extending the trendline class.
+ */
+export class Range extends trendline {
+  /**
+   * Creates a new Range instance.
+   *
+   * @constructor
+   * @param {number} x1 - The x-coordinate of the first point.
+   * @param {number} x2 - The x-coordinate of the second point.
+   * @param {number} y1 - The y-coordinate of the first point.
+   * @param {number} y2 - The y-coordinate of the second point.
+   */
+  constructor(x1, x2, y1, y2) {
+    super(x1, x2, y1, y2);
+  }
+
+  /**
+   * Draws the range on the canvas.
+   *
+   * @param {HTMLCanvasElement} canvas - The canvas element to draw on.
+   */
+  draw(canvas) {
+    const ctx = canvas.getContext('2d');
+    ctx.beginPath();
+
+    // Set fill color based on range direction
+    const fillColor = (this.y1 === this.y2)
+      ? "silver"
+      : (this.y1 < this.y2)
+        ? "rgba(239, 83, 80, 0.3)"
+        : "rgba(38, 166, 154, 0.3)";
+
+    // Draw range rectangle
+    ctx.fillStyle = fillColor;
+    const x = Math.min(this.x1, this.x2);
+    const y = Math.min(this.y1, this.y2);
+    const width = Math.abs(this.x2 - this.x1);
+    const height = Math.abs(this.y2 - this.y1);
+    ctx.fillRect(x, y, width, height);
+
+    // Draw text
+    ctx.beginPath();
+    ctx.fillStyle = 'black';
+    ctx.font = "12px serif";
+    const text = `p1:${this.y1}-p2:${this.y2}-prcent:${Math.abs(this.y2 - this.y1) / 100}`;
+    ctx.fillText(text, x, y);
+
+    if (this.dragEnabled) {
+      this.points.forEach(point => {
+        point.draw(canvas);
+      });
     }
-
-    draw(canvas) {
-        let cnx = canvas.getContext('2d')
-        cnx.beginPath()
-        cnx.fillStyle = (this.y1 === this.y2) ? "silver" : (this.y1 < this.y2) ? "rgba(239,83,80,0.3)" : "rgba(38,166,154,0.3)"
-        cnx.fillRect(this.x1, (this.y1 < this.y2) ? this.y1 : this.y2, Math.abs(this.x2-this.x1), Math.abs(this.y2-this.y1))
-
-        cnx.beginPath()
-        cnx.fillStyle = 'black'
-        cnx.font = "12px serif"
-        const text = `p1:${this.y1}-p2:${this.y2}-prcent:${Math.abs(this.y2-this.y1)/100}`
-        cnx.fillText(text, this.x1, this.y1)
-
-        if (this.dragEnabled) {
-            this.points.forEach(point => {
-                point.draw(canvas)
-            });
-        }
-    }
+  }
 }
