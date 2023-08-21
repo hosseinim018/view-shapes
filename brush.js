cae29be88e79c09c20bae5adc7d78c3d5cf04897
mahdi hosseini <hosseinim018@gmail.com>
2023-08-21 12:49:36 +0330

    update module with docstrig and add comment

--- brush.js
+++ brush.js
@@ -1,42 +1,101 @@
-import {calPoint} from "./math";
-import {Point} from "./point";
+/**
+ * Module representing a Brush object for drawing on a canvas.
+ *
+ * The Brush class provides functionality to draw freehand lines on a canvas.
+ * It tracks the mouse movement to capture the points and renders the lines on the canvas.
+ *
+ * @module Brush
+ * @example
+ * // Create a new instance of the Brush class
+ * const brush = new Brush();
+ *
+ * // Attach event listeners to the canvas
+ * canvas.addEventListener('mousedown', brush.mousedown.bind(brush));
+ * canvas.addEventListener('mousemove', brush.mousemove.bind(brush));
+ * canvas.addEventListener('mouseup', brush.mouseup.bind(brush));
+ *
+ * // Draw the brush strokes on the canvas
+ * brush.draw(canvas);
+ */
 
-export class Brush{
-    dragEnabled = false
-    points = []
-    fillColor = 'rgba(241,18,96,0.36)'
-    size = 2
-    constructor() {
-        this.isDragged = false
-    }
-    mousedown(e){
-        this.isDragged = true
-        let p = calPoint(e)
-        let point = new Point(p.x, p.y)
-        this.points = [point]
+import { calPoint } from "./math";
+import { Point } from "./point";
 
-    }
-    mousemove(e){
-        if (this.isDragged){
-            let p = calPoint(e)
-            let point = new Point(p.x, p.y)
-            this.points.push(point)
-        }
-    }
-    mouseup(e){
-        this.dragEnabled = false
-        this.isDragged = false;
-    }
+/**
+ * Represents a brush object for drawing on a canvas.
+ *
+ * The Brush class provides functionality to draw freehand lines on a canvas.
+ * It tracks the mouse movement to capture the points and renders the lines on the canvas.
+ *
+ * @module Brush
+ */
+export class Brush {
+  /**
+   * Creates a new instance of the Brush class.
+   */
+  constructor() {
+    this.dragEnabled = false;
+    this.points = [];
+    this.fillColor = 'rgba(241, 18, 96, 0.36)';
+    this.size = 2;
+    this.isDragged = false;
+  }
 
-    draw(canvas){
-        let cnx = canvas.getContext('2d')
-        cnx.beginPath()
-        cnx.strokeStyle = this.fillColor
-        cnx.lineWidth = this.size
-        cnx.setLineDash([])
-        this.points.forEach(point => {
-            cnx.lineTo(point.x, point.y)
-        })
-        cnx.stroke();
+  /**
+   * Handles the mousedown event.
+   *
+   * @param {MouseEvent} e - The mousedown event object.
+   */
+  mousedown(e) {
+    this.isDragged = true;
+    const p = calPoint(e);
+    const point = new Point(p.x, p.y);
+    this.points = [point];
+  }
+
+  /**
+   * Handles the mousemove event.
+   *
+   * @param {MouseEvent} e - The mousemove event object.
+   */
+  mousemove(e) {
+    if (this.isDragged) {
+      const p = calPoint(e);
+      const point = new Point(p.x, p.y);
+      this.points.push(point);
     }
+  }
+
+  /**
+   * Handles the mouseup event.
+   *
+   * @param {MouseEvent} e - The mouseup event object.
+   */
+  mouseup(e) {
+    this.dragEnabled = false;
+    this.isDragged = false;
+  }
+
+  /**
+   * Draws the brush strokes on the canvas.
+   *
+   * @param {HTMLCanvasElement} canvas - The canvas element to draw on.
+   */
+  draw(canvas) {
+    const context = canvas.getContext('2d');
+    context.beginPath();
+    context.strokeStyle = this.fillColor;
+    context.lineWidth = this.size;
+    context.setLineDash([]);
+
+    this.points.forEach((point, index) => {
+      if (index === 0) {
+        context.moveTo(point.x, point.y);
+      } else {
+        context.lineTo(point.x, point.y);
+      }
+    });
+
+    context.stroke();
+  }
 }
