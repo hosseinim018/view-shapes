d5a41a037485362d7e74026ce9498e97ce1645d7
mahdi hosseini <hosseinim018@gmail.com>
2023-08-21 12:57:07 +0330

    update module with comments and docstrng

--- traingle.js
+++ traingle.js
@@ -1,8 +1,53 @@
-import {Point} from "./point";
-import {calPoint} from "./math";
+/**
+ * Represents a Triangle object on a canvas.
+ *
+ * The Triangle class provides functionality to create and manipulate a triangle
+ * on a canvas. It allows for dragging of individual points or the entire triangle
+ * within the canvas. The triangle can be drawn with a specified fill color and can
+ * detect whether given coordinates are inside the triangle.
+ *
+ * @module Triangle
+ * @example
+ * // Create a new instance of the Triangle class
+ * const triangle = new Triangle(100, 100, 200, 200, 300, 100);
+ *
+ * // Attach event listeners to the canvas
+ * canvas.addEventListener('mousedown', triangle.mousedown.bind(triangle));
+ * canvas.addEventListener('mousemove', triangle.mousemove.bind(triangle));
+ * canvas.addEventListener('mouseup', triangle.mouseup.bind(triangle));
+ *
+ * // Enable dragging of the triangle
+ * triangle.dragEnabled = true;
+ *
+ * // Draw the triangle on the canvas
+ * triangle.draw(canvas);
+ */
 
+import { Point } from "./point";
+import { calPoint } from "./math";
+
+/**
+ * Represents a Triangle object on a canvas.
+ *
+ * The Triangle class provides functionality to create and manipulate a triangle
+ * on a canvas. It allows for dragging of individual points or the entire triangle
+ * within the canvas. The triangle can be drawn with a specified fill color and can
+ * detect whether given coordinates are inside the triangle.
+ *
+ * @module Triangle
+ */
 export class Triangle {
-  constructor (x1, y1, x2, y2, x3, y3){
+  /**
+   * Creates a new instance of the Triangle class.
+   *
+   * @param {number} x1 - The x-coordinate of the first point.
+   * @param {number} y1 - The y-coordinate of the first point.
+   * @param {number} x2 - The x-coordinate of the second point.
+   * @param {number} y2 - The y-coordinate of the second point.
+   * @param {number} x3 - The x-coordinate of the third point.
+   * @param {number} y3 - The y-coordinate of the third point.
+   */
+  constructor(x1, y1, x2, y2, x3, y3) {
     this.p1 = new Point(x1, y1);
     this.p2 = new Point(x2, y2);
     this.p3 = new Point(x3, y3);
@@ -12,23 +57,35 @@ export class Triangle {
 
     this.points = [this.p1, this.p2, this.p3];
 
-    this.fillColor = 'rgba(241,18,230,0.36)';
+    this.fillColor = 'rgba(241, 18, 230, 0.36)';
   }
 
-  mousedown(e){
-    let p = calPoint(e)
+  /**
+   * Handles the mousedown event.
+   *
+   * @param {MouseEvent} e - The mousedown event object.
+   * @returns {boolean} - Indicates if a point was pressed.
+   */
+  mousedown(e) {
+    const p = calPoint(e);
     const pointPressed = this.points.find(point => point.inPoint(p.x, p.y));
 
-    if (pointPressed){
+    if (pointPressed) {
       pointPressed.isBeingDragged = true;
       this.isDragged = true;
       return true;
     }
+
     return false;
   }
 
-  mousemove(e){
-    let p = calPoint(e)
+  /**
+   * Handles the mousemove event.
+   *
+   * @param {MouseEvent} e - The mousemove event object.
+   */
+  mousemove(e) {
+    const p = calPoint(e);
     const pointDragged = this.points.find(point => point.isBeingDragged);
 
     if (pointDragged) {
@@ -36,23 +93,37 @@ export class Triangle {
     }
   }
 
-  mouseup(e){
-    this.points.forEach(point => { point.isBeingDragged = false; });
+  /**
+   * Handles the mouseup event.
+   *
+   * @param {MouseEvent} e - The mouseup event object.
+   */
+  mouseup(e) {
+    this.points.forEach(point => {
+      point.isBeingDragged = false;
+    });
     this.isDragged = false;
   }
 
-  draw(canvas){
-    let cnx = canvas.getContext('2d')
-    cnx.beginPath()
-    cnx.fillStyle = this.fillColor
-    cnx.moveTo(this.p1.x, this.p1.y)
-    cnx.lineTo(this.p2.x, this.p2.y)
-    cnx.lineTo(this.p3.x, this.p3.y)
-    cnx.closePath()
-    cnx.fill()
+  /**
+   * Draws the triangle on the canvas.
+   *
+   * @param {HTMLCanvasElement} canvas - The canvas element to draw on.
+   */
+  draw(canvas) {
+    const context = canvas.getContext('2d');
+    context.beginPath();
+    context.fillStyle = this.fillColor;
+    context.moveTo(this.p1.x, this.p1.y);
+    context.lineTo(this.p2.x, this.p2.y);
+    context.lineTo(this.p3.x, this.p3.y);
+    context.closePath();
+    context.fill();
 
     if (this.dragEnabled) {
-      this.points.forEach(point => {point.draw(canvas)});
+      this.points.forEach(point => {
+        point.draw(canvas);
+      });
     }
   }
 }
